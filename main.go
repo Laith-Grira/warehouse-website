3c5ccc70f73011a71275b8f2d1e2de5a119110ef
Laith-Grira <lgrira12@gmail.com>
2022-07-03 02:29:58 -0400

    spin up te server

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Starting the application")
-}
 
+	//get the value of the ADDR environment variable
+	addr := os.Getenv("ADDR")
+
+	// Set default port to 6000 if addr doesn't exist
+	if len(addr) == 0 {
+		addr = ":6000"
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/item", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(rw).Encode(map[string]string{"data": "Testing GET method"})
+	}).Methods("GET")
+
+	fmt.Printf("Starting server at port%v\n", addr)
+	if err := http.ListenAndServe(addr, router); err != nil {
+		log.Fatal(err)
+	}
+}
